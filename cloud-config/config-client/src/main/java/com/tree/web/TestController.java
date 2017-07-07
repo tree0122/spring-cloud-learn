8b1203befe8c2d5fadad30a3361117b7a53e5789
cuiyaohua <cuiyaohua@mobike.com>
2017-07-07 13:31:35 +0800

    cloud config update

--- cloud-config/config-client/src/main/java/com/tree/web/TestController.java
+++ cloud-config/config-client/src/main/java/com/tree/web/TestController.java
@@ -1,7 +1,9 @@
 package com.tree.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,15 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by smafi on 2017/6/19.
  */
-@RefreshScope
+//@RefreshScope
 @RestController
 public class TestController {
 
-    @Value("${from}")
+    @Value("${info.profile}")
     private String from;
+
+    @Autowired
+    private Environment env;
 
     @RequestMapping(value = "/from", method = RequestMethod.GET)
     public String from(){
+        String[] profiles = env.getActiveProfiles();
+        System.out.println(profiles);
         return this.from;
     }
 }
